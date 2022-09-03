51f6f03dc7e671d3ef719d79f0c36e0a58140f73
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-09-03 12:53:14 +0600

    load news

--- js/newsData.js
+++ js/newsData.js
@@ -6,8 +6,7 @@ const loadCategories = () => {
 
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("categoryContainer");
-  categories.forEach(category => {
-    console.log(category);
+  categories.forEach((category) => {
     const categoryli = document.createElement("li");
     categoryli.classList.add();
     categoryli.innerHTML = `
@@ -18,6 +17,58 @@ const displayCategories = (categories) => {
   });
 };
 
-const loadNews = 
+const loadNews = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayNews(data.data);
+};
 
+const displayNews = (newsContent) => {
+  const newsContainer = document.getElementById("newsCard");
+  newsContent.forEach((news) => {
+    console.log(news);
+    const contentDiv = document.createElement("div");
+    contentDiv.innerHTML = `
+        <div class="card mx-auto" style="max-width: 720px">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${
+              news.thumbnail_url
+            }" class="img-fluid rounded-start" alt="..." />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${news.title}</h5>
+              <p class="card-text">
+                ${news.details}
+              </p>
+              <div class="d-flex ">
+                <div class="col-md-4 col-sm-6 d-flex align-items-center">
+                  <img width="${50}" src="${news.author.img}" alt="John Smith">
+                  <h5 class="ms-3">${
+                    news.author.name ? news.author.name : "not available"
+                  }</h5>
+              </div>
+              <p class="card-text mx-2">
+                <small class="text-muted"> <strong>Published Date</strong> : ${
+                  news.author.published_date
+                }</small>
+              </p>
+              <p class="card-text mx-2 fw-bold">
+                <small class="text-muted">total view ${
+                  news.total_view ? news.total_view : "not available"
+                }</small>
+              </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    newsContainer.appendChild(contentDiv);
+  });
+};
+
+loadNews("02");
 loadCategories();
